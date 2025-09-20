156775cefff92794d47743a3ab81788c6c2e2b0e
ubayDext <ubaydillah.tech@gmail.com>
2025-09-20 10:12:00 +0700

    sertifikat

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('students/classroom/{classroom_id}', [StudentController::class, 'byClassroom']);
     Route::get('students/ekskul/{ekskul_id}', [StudentController::class, 'byEkskul']);
     Route::get('/students/with-sertifikat', [StudentController::class, 'withSertifikat']);
+    Route::apiResource('students', StudentController::class)->except(['show']);
+Route::get('students/{id}', [StudentController::class, 'show'])->whereNumber('id');
 
 
 
